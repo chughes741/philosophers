349069fbf40bfba23a0ccaaed7510743238ddbf6
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-06 15:34:03 -0400

    Added some mock fns

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,28 +6,30 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 14:50:16 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/06 15:00:03 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/06 15:15:22 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
-void	*myThreadFun(void *vargp)
+void	*init_thread(void *varg)
 {
     sleep(1);
-    printf("Printing GeeksQuiz from Thread \n");
-	printf("%p\n", vargp);
-    return NULL;
+    printf("Printing from a thread\n");
+	printf("%p\n", varg);
+    return (NULL);
 }
 
-int	main()
+int	main(int argc, char *argv[])
 {
 	t_data  *data;
 
     data = get_data();
+    init_data(argc, argv);
     data->philos = malloc(sizeof(t_philo *));
+
     printf("Before Thread\n");
-    pthread_create(&data->philos->id, NULL, myThreadFun, NULL);
+    pthread_create(&data->philos->id, NULL, init_thread, NULL);
     pthread_join(data->philos->id, NULL);
     printf("After Thread\n");
     exit(0);
