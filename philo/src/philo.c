cb0213ce28314890818a61625d81c17daf1e9479
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-06 14:53:25 -0400

    Added some thread test code

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,13 +6,28 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 14:50:16 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/06 14:50:56 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/06 14:52:12 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
-int	main()
+void	*myThreadFun(void *vargp)
 {
-	return (0);
+    sleep(1);
+    printf("Printing GeeksQuiz from Thread \n");
+	printf("%p\n", vargp);
+    return NULL;
+}
+
+int	main(int argc, char *argv[])
+{
+	pthread_t	thread_id;
+
+    
+    printf("Before Thread\n");
+    pthread_create(&thread_id, NULL, myThreadFun, NULL);
+    pthread_join(thread_id, NULL);
+    printf("After Thread\n");
+    exit(0);
 }
