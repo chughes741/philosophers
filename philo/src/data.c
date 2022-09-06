48466f7f0028194ae5df0f624b9a97132704c18f
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-06 15:33:52 -0400

    Added init_data

--- philo/src/data.c
+++ philo/src/data.c
@@ -6,12 +6,28 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 12:37:14 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/06 14:53:06 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/06 15:09:18 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
+// Initialize data struct with inputs
+void	init_data(int argc, char *argv[])
+{
+	t_data	*data;
+
+	data = get_data();
+	data->n_philos = atoi(argv[1]);
+	data->time_die = atoi(argv[2]);
+	data->time_eat = atoi(argv[3]);
+	data->time_sleep = atoi(argv[4]);
+	if (argc == 6)
+		data->n_eat = atoi(argv[5]);
+	return ;
+}
+
+// Singleton constructor
 t_data	*get_data(void)
 {
 	static t_data	*data = NULL;
@@ -24,6 +40,7 @@ t_data	*get_data(void)
 	return (data);
 }
 
+// Data struct deconstructor
 void	del_data(void)
 {
 	t_data	*data;
