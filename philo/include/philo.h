d11cbd18549397e12d851807d2cd3cb2a07c96b0
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-06 12:14:57 -0400

    Added basic structs

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.c                                          :+:      :+:    :+:   */
+/*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/07 14:50:16 by chughes           #+#    #+#             */
-/*   Updated: 2022/07/07 14:50:56 by chughes          ###   ########.fr       */
+/*   Created: 2022/09/06 14:50:16 by chughes           #+#    #+#             */
+/*   Updated: 2022/09/06 14:50:56 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,14 @@
 # include <sys/time.h> //? gettimeofday
 # include <pthread.h> //? pthread_create, pthread_detach, pthread_join, 
 //? pthread_mutex_init, pthread_mutex_destroy, pthread_mutex_lock, pthread_mutex_unlock
+
+typedef struct s_data {
+	t_philo	*philos;
+}			t_data;
+
+typedef struct s_philo {
+	int	id;
+	int	state;
+}		t_philo;
 
 #endif
