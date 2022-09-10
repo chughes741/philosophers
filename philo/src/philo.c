4f949283bac67c4a2bde405fcd997247b4d6910e
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-10 14:17:00 -0400

    Added basic error checks to main

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 14:50:16 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/10 13:54:25 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/10 14:15:56 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,14 @@
 
 void	*init_thread(void *varg)
 {
-    sleep(1);
-    printf("Printing from a thread\n");
-	printf("%p\n", varg);
     return (NULL);
 }
 
 int	main(int argc, char *argv[])
 {
-	t_data  *data;
-
     if (argc != 5 || argc != 6)
-        exit(1);
-    data = get_data();
+        exit_error("Wrong number of arguments");
     init_data(argc, argv);
-    data->philos = malloc(sizeof(t_philo *));
-
-    printf("Before Thread\n");
-    pthread_create(&data->philos->id, NULL, init_thread, NULL);
-    pthread_join(data->philos->id, NULL);
-    printf("After Thread\n");
+    del_data();
     exit(0);
 }
