101116ece9d4cd4dceabd57fab388779c3544e4f
Cole Hughes <95713039+chughes741@users.noreply.github.com>
2022-09-10 20:22:30 -0400

    Typo in if statement

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,20 +6,15 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 14:50:16 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/10 14:18:07 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/10 20:21:44 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
-void	*init_thread(void *varg)
-{
-    return (NULL);
-}
-
 int	main(int argc, char *argv[])
 {
-    if (argc != 5 || argc != 6)
+    if (argc != 5 && argc != 6)
         exit_error("Wrong number of arguments");
     init_data(argc, argv);
     if (!get_data())
