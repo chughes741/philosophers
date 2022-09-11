d292fac387795524d3f4784dbc539102b37e52ea
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-11 12:31:40 -0400

    Removed unneeded guard

--- philo/src/philo.c
+++ philo/src/philo.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 14:50:16 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/10 20:21:44 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/11 12:31:32 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,6 @@ int	main(int argc, char *argv[])
     if (argc != 5 && argc != 6)
         exit_error("Wrong number of arguments");
     init_data(argc, argv);
-    if (!get_data())
-        exit_error("Error initializing data");
     init_philos();
     del_data();
     exit(0);
