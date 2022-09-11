dbc1f0f0f88e8aa04294730d9ef5588077f529e3
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-11 14:45:38 -0400

    Added philo counter in to live

--- philo/src/init_philos.c
+++ philo/src/init_philos.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 20:06:54 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/11 14:08:31 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/11 14:12:12 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,10 @@
 // Thread runtime for philos
 void	*live(void *ptr)
 {
+	int	philo_id;
+
+	philo_id = *(int *)ptr;
+	printf("Inside %i\n", philo_id);
 	while (ptr) //TODO lifetime loop
 	{
 		return (NULL);
@@ -30,10 +34,9 @@ void	init_philos(void)
 
 	data = get_data();
 	i = 0;
-	printf("n_philos: %i\n", data->n_philos);
 	while (++i <= data->n_philos)
 	{
-		if (pthread_create(&data->philos[i].id, NULL, &live, NULL))
+		if (pthread_create(&data->philos[i].id, NULL, &live, &i))
 			exit_error("Error creating threads");
 	}
 	i = 0;
