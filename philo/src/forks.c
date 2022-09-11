59ac2c6a4569ab4655869654e2d99bb05d2194c3
Cole Hughes <95713039+chughes741@users.noreply.github.com>
2022-09-10 21:40:39 -0400

    Added mutex inits and destroys

--- philo/src/forks.c
+++ philo/src/forks.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 21:31:38 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/10 21:32:24 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/10 21:40:26 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,27 @@
 
 void	init_forks(void)
 {
+	t_data	*data;
+	int		i;
+
+	data = get_data();
+	i = -1;
+	while (++i < data->n_philos)
+	{
+		if (pthread_mutex_init(data->forks[i], NULL))
+			exit_error("Error making forks");
+	}
 	return ;
 }
 
 void	destroy_forks(void)
 {
+	t_data	*data;
+	int		i;
+
+	data = get_data();
+	i = -1;
+	while (++i < data->n_philos)
+		pthread_mutex_destroy(data->forks[i]);
 	return ;
 }
