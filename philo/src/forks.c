f20ea8a0687574585679b68dc873253a4ea67abe
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-12 11:54:56 -0400

    Added state change mutex to actions

--- philo/src/forks.c
+++ philo/src/forks.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 21:31:38 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/11 14:06:18 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/12 11:51:26 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ void	init_forks(void)
 
 	data = get_data();
 	data->forks = ft_calloc(data->n_philos + 1, sizeof(pthread_mutex_t));
+	if (pthread_mutex_init(&data->change_state, NULL))
+		exit_error("Error initializing mutex");
 	i = -1;
 	while (++i < data->n_philos)
 	{
@@ -36,6 +38,7 @@ void	destroy_forks(void)
 	int		i;
 
 	data = get_data();
+	pthread_mutex_destroy(&data->change_state);
 	i = -1;
 	while (++i < data->n_philos)
 		pthread_mutex_destroy(&data->forks[i]);
--- philo/src/sleep.c
+++ philo/src/sleep.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 14:19:30 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/11 15:44:21 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/12 11:53:16 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ void	philo_sleep(int id)
 
 	data = get_data();
 	time = get_time() - data->start_time;
+	pthread_mutex_lock(&data->change_state);
 	printf("%lims %i is sleeping\n", time , id);
+	pthread_mutex_unlock(&data->change_state);
 	usleep(data->time_sleep * 1000);
 	return ;
 }
