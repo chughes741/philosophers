3f069500e022d2f959e764fab4149eff3cb63746
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-14 12:58:51 -0400

    Norm fix

--- philo/src/sleep.c
+++ philo/src/sleep.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 14:19:30 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/14 12:56:32 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/14 12:58:43 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	philo_sleep(int id)
 	t_data	*data;
 
 	data = get_data();
-	smart_sleep(data->time_sleep);	
+	smart_sleep(data->time_sleep);
 	print_action(P_SLEEP, id);
 	return ;
 }
