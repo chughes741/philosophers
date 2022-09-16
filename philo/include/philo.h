bf513813f6fcf7956f72e812fa985bb70194f703
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-16 11:18:06 -0400

    Removed unused comments from header

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,21 +6,19 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 14:50:16 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/13 16:13:45 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/16 11:17:42 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-# include <string.h>	//? memset
-# include <stdio.h>		//? printf
-# include <stdlib.h>	//? malloc, free
-# include <unistd.h>	//? usleep
-# include <sys/time.h>	//? gettimeofday
-# include <pthread.h>	//? pthread_create, pthread_detach, pthread_join, 
-						//? pthread_mutex_init, pthread_mutex_destroy,
-						//? pthread_mutex_lock, pthread_mutex_unlock
+# include <string.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/time.h>
+# include <pthread.h>
 # include <stdbool.h>
 
 # define P_FORK		0
