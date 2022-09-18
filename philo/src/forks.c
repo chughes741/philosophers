5651286493e54415cabc2b4994d3a78dee315de9
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-09-18 16:25:52 -0400

    Fixed error with number of forks

--- philo/src/forks.c
+++ philo/src/forks.c
@@ -6,7 +6,7 @@
 /*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/10 21:31:38 by chughes           #+#    #+#             */
-/*   Updated: 2022/09/12 12:28:47 by chughes          ###   ########.fr       */
+/*   Updated: 2022/09/18 16:24:15 by chughes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ void	init_forks(void)
 	int		i;
 
 	data = get_data();
-	data->forks = ft_calloc(data->n_philos + 1, sizeof(pthread_mutex_t));
+	data->forks = ft_calloc(data->n_philos + 2, sizeof(pthread_mutex_t));
 	if (pthread_mutex_init(&data->action, NULL))
 		exit_error("Error initializing mutex");
 	i = -1;
-	while (++i < data->n_philos)
+	while (++i <= data->n_philos)
 	{
 		if (pthread_mutex_init(&data->forks[i], NULL))
 			exit_error("Error making forks");
@@ -40,7 +40,7 @@ void	destroy_forks(void)
 	data = get_data();
 	pthread_mutex_destroy(&data->action);
 	i = -1;
-	while (++i < data->n_philos)
+	while (++i <= data->n_philos)
 		pthread_mutex_destroy(&data->forks[i]);
 	free(data->forks);
 	return ;
